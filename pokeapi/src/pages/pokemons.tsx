d8757f2cbeea162c76c4fee33df8e97db848dca3
EdisonAlzate <edisonalzate11@gmail.com>
2023-03-13 22:47:12 -0500

    diseño main

--- pokeapi/src/pages/pokemons.tsx
+++ pokeapi/src/pages/pokemons.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+
+import pokemonPic from "./../assets/001.png"
+import styles from './pokemons.module.css'
 
 const Pokemons = () => {
 
     const [query, setQuery] = useState("")
     return (
         <>
-        <Header query={query} setQuery={setQuery}/>
+            <Header query={query} setQuery={setQuery} />
             <main>
-                <h1>POKEMOS</h1>
+                <nav>
+                    <Link className={styles.listItem} to="/">                        
+                    </Link>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={pokemonPic} alt="BULBASAUR" />
+                        <div className={styles.listItemText} >
+                            <span>Bulbasaur</span>
+                            <span>001</span>
+                        </div>
+                    </Link>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={pokemonPic} alt="BULBASAUR" />
+                        <div className={styles.listItemText} >
+                            <span>Ivisaur</span>
+                            <span>002</span>
+                        </div>
+                    </Link>
+                    <Link className={styles.listItem} to="/">
+                        <img className={styles.listItemIcon} src={pokemonPic} alt="BULBASAUR" />
+                        <div className={styles.listItemText} >
+                            <span>Velbuasaur</span>
+                            <span>003</span>
+                        </div>
+                    </Link>
+                </nav>
             </main>
-            <Footer/>
+            <Footer />
         </>
     )
 }
